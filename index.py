2d6a2846ef5e755bd64aff2d2164ff9a99cbe472
pincock <pincock.luke@gmail.com>
2025-01-16 13:17:20 -0700

    changes to use shapes instead of images

--- index.py
+++ index.py
@@ -1,1 +1,119 @@
 # this will be my snake game
+
+import pygame
+import time
+import random
+
+# Initialize the game
+pygame.init()
+
+# Set up display
+width, height = 600, 400
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game')
+
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+
+# Game variables
+snake_block = 10
+snake_speed = 15
+
+# Clock
+clock = pygame.time.Clock()
+
+# Font
+font_style = pygame.font.SysFont(None, 50)
+
+# Load apple image
+apple_image = pygame.image.load('apple.png')
+apple_image = pygame.transform.scale(apple_image, (snake_block, snake_block))
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    win.blit(mesg, [width / 6, height / 3])
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = width / 2
+    y1 = height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_list = []
+    length_of_snake = 1
+
+    foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close:
+            win.fill(black)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        win.fill(black)
+        pygame.draw.circle(win, red, (foodx + snake_block // 2, foody + snake_block // 2), snake_block // 2)
+        snake_head = []
+        snake_head.append(x1)
+        snake_head.append(y1)
+        snake_list.append(snake_head)
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                game_close = True
+
+        for x in snake_list:
+            pygame.draw.rect(win, green, [x[0], x[1], snake_block, snake_block])
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+            length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
