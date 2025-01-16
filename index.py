523fa600c19c0a7a59c7a6996b2f083a554c9767
pincock <pincock.luke@gmail.com>
2025-01-16 13:44:54 -0700

    makes it so the snake image moves

--- index.py
+++ index.py
@@ -39,14 +39,12 @@ def gameLoop():
     x1 = width / 2
     y1 = height / 2
 
-    x1_change = 0
+    # Start moving to the right
+    x1_change = snake_block
     y1_change = 0
 
     snake_list = []
-    length_of_snake = 1
-
-    # Initial position of the snake
-    snake_list.append([x1, y1])
+    length_of_snake = 3  # Start with a longer snake
 
     foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
     foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
@@ -72,23 +70,25 @@ def gameLoop():
             if event.type == pygame.QUIT:
                 game_over = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+                if event.key == pygame.K_LEFT and x1_change == 0:
                     x1_change = -snake_block
                     y1_change = 0
-                elif event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT and x1_change == 0:
                     x1_change = snake_block
                     y1_change = 0
-                elif event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP and y1_change == 0:
                     y1_change = -snake_block
                     x1_change = 0
-                elif event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN and y1_change == 0:
                     y1_change = snake_block
                     x1_change = 0
 
         if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
             game_close = True
+
         x1 += x1_change
         y1 += y1_change
+
         win.fill(black)
         
         # Draw the walls
@@ -100,16 +100,22 @@ def gameLoop():
         # Draw the fruit as a circle
         pygame.draw.circle(win, red, (foodx + snake_block // 2, foody + snake_block // 2), snake_block // 2)
         
+        # Update snake position
+        snake_head = [x1, y1]
+        snake_list.append(snake_head)
+        if len(snake_list) > length_of_snake:
+            del snake_list[0]
+
+        # Check for collision with itself
+        for x in snake_list[:-1]:
+            if x == snake_head:
+                game_close = True
+
         # Draw the snake in white
         for x in snake_list:
             pygame.draw.rect(win, white, [x[0], x[1], snake_block, snake_block])
 
         pygame.display.update()
-
-        # Check collision with walls
-        if (x1 < wall_thickness or x1 >= width - wall_thickness or
-            y1 < wall_thickness or y1 >= height - wall_thickness):
-            game_close = True
 
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
@@ -117,8 +123,6 @@ def gameLoop():
             length_of_snake += 1
 
         clock.tick(snake_speed)
-
-        print("Snake position:", snake_list)
 
     pygame.quit()
     quit()
